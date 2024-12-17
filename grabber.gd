16ac5b6fc765b15070e1d354712a7128f2128f04
Ænthroppe <jamison.bassett@gmail.com>
2024-12-17 18:04:09 +0100

    cane droppable only after prev one landed

--- grabber.gd
+++ grabber.gd
@@ -3,15 +3,18 @@ extends Node2D
 @onready
 var candy_cane_base = preload("res://candy_cane.tscn")
 var current_cane
+var cane_ready := false
+@onready
+var cane_parent = $CaneSpawnPoint
 
 func _ready() -> void:
 	spawn_cane()
 	pass
 
 func _input(event: InputEvent) -> void:
-	if InputMap.action_has_event("drop", event) and event.is_pressed():
+	if InputMap.action_has_event("drop", event) and event.is_pressed() and cane_ready:
+		cane_ready = false
 		current_cane.fall()
-		spawn_cane()
 
 
 func _process(delta: float) -> void:
@@ -21,4 +24,12 @@ func _process(delta: float) -> void:
 
 func spawn_cane() -> void:
 	current_cane = candy_cane_base.instantiate() 
-	add_child(current_cane)
+	current_cane.get_child(0).body_entered.connect(on_cane_landed)
+	cane_parent.add_child(current_cane)
+	cane_ready = true
+
+func on_cane_landed(body: Node) -> void :
+	print("hewwo")
+	if !cane_ready:
+		print("spawning")
+		call_deferred("spawn_cane")
